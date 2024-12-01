3760ebf9f1d4e38da8f4e9a4541a685341d55607
Santiagorodriguezgalviz <santiagorodriguezgalviz@gmail.com>
2024-12-01 16:02:43 -0500

    refactor: move firebase config to env variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVShfUiSzKEIz_xbC22hQ-KjVmA7aPq6k",
-  authDomain: "portafolio-70ae4.firebaseapp.com",
-  projectId: "portafolio-70ae4",
-  storageBucket: "portafolio-70ae4.firebasestorage.app",
-  messagingSenderId: "380997181256",
-  appId: "1:380997181256:web:8b98f6d709e7a2f9dd1d7d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
